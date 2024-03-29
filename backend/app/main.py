02be170f16204ea79b52d1ad2f802e27ab465a3e
jinyang628 <chenjinyang4192@gmail.com>
2024-03-28 23:19:11 -0700

    Update

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
 
-app.get("/")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+
+
+@app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+class Message(BaseModel):
+    message: str
+    
+@app.post("/api/post_message") 
+def post_message(message: Message): 
+    print(message)
+    return message
