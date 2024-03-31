567b8203dae69dcd2e185239d9672d2b47a76524
jinyang628 <81862152+jinyang628@users.noreply.github.com>
2024-03-30 22:29:45 -0700

    Update

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,8 +3,10 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from app.types.generate_riddle import GenerateRiddleResponse
 
+from app.types.get_hint import GetHintResponse
 from app.types.attractions import Attraction
 from app.api.generate_riddle import send_riddle_request
+from app.api.get_hint import send_hint_request
 
 app = FastAPI()
 
@@ -16,6 +18,7 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
+riddle_dict: dict[str, str] = {}
 
 @app.get("/")
 def read_root():
@@ -23,6 +26,13 @@ def read_root():
 
     
 @app.post("/api/generate_riddle") 
-async def post_message(attraction: Attraction): 
-    response: GenerateRiddleRespone = await send_riddle_request(location=attraction.name)
+async def generate_riddle(attraction: Attraction): 
+    response: GenerateRiddleResponse = await send_riddle_request(location=attraction.name)
+    riddle: str = response["result"]
+    riddle_dict[attraction.name] = riddle
+    return riddle
+
+@app.post("/api/get_hint")
+async def get_hint(attraction: Attraction):
+    response: GetHintResponse = await send_hint_request(location=attraction.name, riddle=riddle_dict[attraction.name])
     return response["result"]
